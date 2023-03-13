a7b6e247f5e8d1e8c379728a75103cb3b548742f
Louis Perry <louis.perry3@gmail.com>
2023-03-13 16:15:20 -0400

    beginning work for firebase

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmuPukMfF2oYof5QUh8dQxzi2GWS7-lBQ",
+  authDomain: "fir-blog-4d6ff.firebaseapp.com",
+  projectId: "fir-blog-4d6ff",
+  storageBucket: "fir-blog-4d6ff.appspot.com",
+  messagingSenderId: "251393882002",
+  appId: "1:251393882002:web:e9a72cf330c1fe33eb734b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import './Homepage.css'
+
+
+function Homepage() {
+  return (
+    <div>Homepage</div>
+  )
+}
+
+export default Homepage
